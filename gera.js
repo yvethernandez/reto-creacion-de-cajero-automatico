148c606513acf7ddf056b6102299e5ea30ddc34c
yvethernandez <yvet_mayresa@hotmail.com>
2021-10-31 19:18:54 -0600

    first cajero

---
+++ gera.js
@@ -0,0 +1,62 @@
+function consulta_de_saldo(){
+
+    var opcion = 0;
+    var saldoa = 750;
+    var retiro = 0;
+
+    opcion = parseInt(prompt("Si dijiste consulta saldo actual, ingresa 1: "));
+                 
+   if(opcion == 1){
+    document.write("Tu saldo actual es: " + saldoa);
+   }
+   if(opcion == 2){    
+  
+    while (retiro>saldoa || retiro==0 || (retiro%50) != 0 ) {
+     retiro=parseInt(prompt("Cuanto vas a retirar"));
+       }
+          
+    document.write("Tu saldo actual es: " + saldoa);
+      }
+}
+ 
+ function retiro_de_saldo(){
+
+    var opcion = 0;
+    var saldoa = 750;
+    var retiro = 0;
+
+    opcion = parseInt(prompt("Si dijiste retiro de saldo, ingresa 2: "));
+                 
+   if(opcion == 2){    
+    retiro = parseInt(prompt("Ingresa saldo que desea retirar: "));
+     saldoa = saldoa - retiro;
+       document.write("Tu saldo actual es: " + saldoa);
+       if(saldoa > 990){
+        alert("Tu cuenta no puede tener mas de $990");
+    }
+    if(saldoa < 10){
+        alert("Tu cuenta no puede tener menos de $10");
+    }
+     }
+    }
+    
+    function deposito(){
+
+        var opcion = 0;
+        var saldoa = 750;
+        var deposito = 0;
+
+        opcion = parseInt(prompt("Si dijiste deposito de  saldo, ingresa 3: "));
+
+        if(opcion == 3){
+            deposito = parseInt(prompt("Cuanto depositas: "));
+            saldoa = saldoa + deposito;
+            document.write("Tu saldo actual es: " + saldoa);
+        if(saldoa > 990){
+            alert("Tu cuenta no puede tener mas de $990");
+        }
+        if(saldoa < 10){
+            alert("Tu cuenta no puede tener menos de $10");
+        }
+ }
+     }
---
+++ noreal.js
@@ -0,0 +1,153 @@
+
+function mensaje(){
+    let ingreso = "";
+
+    ingreso = prompt("Desea ingresar a cuentas");
+    if(ingreso == "si"){
+      alert("De click en aceptar y elija cuenta");
+    
+    }else{
+        alert("Gracias por haber visitado tu cajero Mx");
+    }
+}
+ // cuenta mali ____________________________________
+function parteDos(){
+    var contraseñaGuardada = 3905;
+    var contraseñaEscrita = 0;
+    var contador = 0;
+    
+    while(contraseñaGuardada!=contraseñaEscrita){
+         contraseñaEscrita = parseInt(prompt("Ingrese contraseña"));
+         contador = contador + 1;
+        if (contador ==3){
+           break;
+     }
+     if(contador == 2 && contraseñaGuardada!=contraseñaEscrita){
+      alert("Ultima oportunidad, si ingresas nuevamente contraseña incorrecta tu cuenta se bloquera");
+     }    
+    }
+    if (contraseñaEscrita==contraseñaGuardada){
+        swal("contraseña exitosa!", "Da click en ok y luego en acceder!", "success");
+       }
+       else{
+        swal("Te lo dije!", "Tarjeta bloqueada!", "error");
+    }
+         }
+
+       // cuenta maui ________________________________________________________________________
+    function cuentaSegunda(){
+        var contraseñaGuardada = 1501;
+        var contraseñaEscrita = 0;
+        var contador = 0;
+
+        while(contraseñaGuardada!=contraseñaEscrita){
+            contraseñaEscrita = parseInt(prompt("Ingrese contraseña"));
+            contador = contador + 1;
+           if (contador ==3){
+              break;
+        }
+        if(contador == 2 && contraseñaGuardada!=contraseñaEscrita){
+         alert("Ultima oportunidad, si ingresas nuevamente contraseña incorrecta tu cuenta se bloquera");
+        }    
+       }
+       if (contraseñaEscrita==contraseñaGuardada){
+           swal("contraseña exitosa!", "Da click en ok y luego en acceder!", "success");
+          }
+          else{
+           swal("Te lo dije!", "Tarjeta bloqueada!", "error");
+       }
+  }
+
+     // cuenta adoni _______________________________________________
+  function cuentaTercera(){
+    var contraseñaGuardada = 3816;
+    var contraseñaEscrita = 0;
+    var contador = 0;
+
+    while(contraseñaGuardada!=contraseñaEscrita){
+        contraseñaEscrita = parseInt(prompt("Ingrese contraseña"));
+        contador = contador + 1;
+       if (contador ==3){
+          break;
+    }
+    if(contador == 2 && contraseñaGuardada!=contraseñaEscrita){
+     alert("Ultima oportunidad, si ingresas nuevamente contraseña incorrecta tu cuenta se bloquera");
+    }    
+   }
+   if (contraseñaEscrita==contraseñaGuardada){
+       swal("contraseña exitosa!", "Da click en ok y luego en acceder!", "success");
+      }
+      else{
+       swal("Te lo dije!", "Tarjeta bloqueada!", "error");
+   }
+  
+}
+// cuenta lina _______________________________________________
+function cuentaCuarta(){
+    var contraseñaGuardada = 3816;
+    var contraseñaEscrita = 0;
+    var contador = 0;
+
+    while(contraseñaGuardada!=contraseñaEscrita){
+        contraseñaEscrita = parseInt(prompt("Ingrese contraseña"));
+        contador = contador + 1;
+       if (contador ==3){
+          break;
+    }
+    if(contador == 2 && contraseñaGuardada!=contraseñaEscrita){
+     alert("Ultima oportunidad, si ingresas nuevamente contraseña incorrecta tu cuenta se bloquera");
+    }    
+   }
+   if (contraseñaEscrita==contraseñaGuardada){
+       swal("contraseña exitosa!", "Da click en ok y luego en acceder!", "success");
+      }
+      else{
+       swal("Te lo dije!", "Tarjeta bloqueada!", "error");
+}
+}
+//cuenta jonh ___________________________________________________
+function cuentaQuinta(){
+    var contraseñaGuardada = 3711;
+    var contraseñaEscrita = 0;
+    var contador = 0;
+
+    while(contraseñaGuardada!=contraseñaEscrita){
+        contraseñaEscrita = parseInt(prompt("Ingrese contraseña"));
+        contador = contador + 1;
+       if (contador ==3){
+          break;
+    }
+    if(contador == 2 && contraseñaGuardada!=contraseñaEscrita){
+     alert("Ultima oportunidad, si ingresas nuevamente contraseña incorrecta tu cuenta se bloquera");
+    }    
+   }
+   if (contraseñaEscrita==contraseñaGuardada){
+       swal("contraseña exitosa!", "Da click en ok y luego en acceder!", "success");
+      }
+      else{
+       swal("Te lo dije!", "Tarjeta bloqueada!", "error");
+}
+}
+//cuenta gera ___________________________________________________
+function cuentaSexta(){
+    var contraseñaGuardada = 1331;
+    var contraseñaEscrita = 0;
+    var contador = 0;
+
+    while(contraseñaGuardada!=contraseñaEscrita){
+        contraseñaEscrita = parseInt(prompt("Ingrese contraseña"));
+        contador = contador + 1;
+       if (contador ==3){
+          break;
+    }
+    if(contador == 2 && contraseñaGuardada!=contraseñaEscrita){
+     alert("Ultima oportunidad, si ingresas nuevamente contraseña incorrecta tu cuenta se bloquera");
+    }    
+   }
+   if (contraseñaEscrita==contraseñaGuardada){
+       swal("contraseña exitosa!", "Da click en ok y luego en acceder!", "success");
+      }
+      else{
+       swal("Te lo dije!", "Tarjeta bloqueada!", "error");
+}
+}
